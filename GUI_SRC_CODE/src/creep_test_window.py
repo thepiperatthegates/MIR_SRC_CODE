f8d927f372cb9bdac2aefc44faf91497d381bc02
Hijazi Hakkim <hijaziaziz@gmail.com>
2025-10-20 14:05:48 +0200

    creep test pt3

--- GUI_SRC_CODE/src/creep_test_window.py
+++ GUI_SRC_CODE/src/creep_test_window.py
@@ -24,7 +24,28 @@ from creep_test_gui import Ui_CreepTestGUI
 class CreepTestGUI(QMainWindow, Ui_CreepTestGUI):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        self.setupUi(self)\
+            
+            
+        ################################HINT TYPE###################################################
+        
+        
+        self.textbox_offset_1.setPlaceholderTex("Enter +-480mA")
+        self.textbox_offset_2.setPlaceholderTex("Enter +-480mA")
+        
+        self.textbox_direction1_1.setPlaceholderText("Direction 1")
+        self.textbox_direction2_1.setPlaceholderText("Direction 2")
+        self.textbox_direction1_2.setPlaceholderText("Direction 1")
+        self.textbox_direction2_2.setPlaceholderText("Direction 2")
+        
+        self.textbox_vector_time_1.setPlaceholderText("for vector 1")
+        self.textbox_vector_time_2.setPlaceholderText("for vector 2")
+        
+        self.textbox_sampling_rate.setPlaceholderText("specify the fast sampling rate!")
+        self.textbox_sampling_time.setPlaceholderText("counting from 0s")
+        
+        self.textbox_standard_sampling_rate.setPlaceholderText("10000")
+        #######################################################################################
 
 
 
