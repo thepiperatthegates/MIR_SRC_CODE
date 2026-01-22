8049d3dbc073d131e7cc0881fe0c0aa5259bb916
Hijazi Hakkim <hijaziaziz@gmail.com>
2026-01-22 18:29:52 +0100

    fixed offsets bug inside the analyse window tab

--- STM_SRC_CODE/again_try/CM4/build/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "again_try_CM4.map"
-  )
-endif()
--- STM_SRC_CODE/again_try/build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "/Users/mac/Documents/again_try/CM4/build"
-  "/Users/mac/Documents/again_try/CM7/build"
-  )
-endif()
